6f9c97a2eb3ba018afcce5a64e519c3314c0cc2d
mandeepkanoujiya <mandeepdk123@gmail.com>
2020-06-01 07:46:28 +0530

    Updates

---
+++ precache-manifest.104fe279dd7d5a9286a73a021d786dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f741ee3c9be2086981fd3732dab49c1b",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "4de8e932110214e35df3",
+    "url": "/mywebsite/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "cf8e5a09e23abd3b9f5f",
+    "url": "/mywebsite/static/css/main.e834267f.chunk.css"
+  },
+  {
+    "revision": "4de8e932110214e35df3",
+    "url": "/mywebsite/static/js/2.223f9e67.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/mywebsite/static/js/2.223f9e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8e5a09e23abd3b9f5f",
+    "url": "/mywebsite/static/js/main.c41873e0.chunk.js"
+  },
+  {
+    "revision": "c15c4764f79dde6560f3",
+    "url": "/mywebsite/static/js/runtime-main.2438cc28.js"
+  }
+]);
